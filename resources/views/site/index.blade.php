43f1e5a700131d7f92982f6577a18120b1adf32e
henrique <hlopessantos52@gmail.com>
2023-07-16 21:59:41 -0300

    create migration and add form to index.blade.php

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,3 +7,10 @@
 <div>
     <h1>Biblioteca</h1>
 </div>
+<div>
+    <h2>Formulário</h2>
+    <p>Formulario para emprestar um livro</p>
+    @component('site.layouts._components.form_basic', ['classe' => 'borda-preta', 'Button' => 'Emprestar'])
+
+    @endcomponent
+</div>
