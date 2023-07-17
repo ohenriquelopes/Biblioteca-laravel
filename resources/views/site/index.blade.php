d5b9be3dbdd43809d77b1b363ac9a8c84aa2c292
henrique <hlopessantos52@gmail.com>
2023-07-16 21:37:28 -0300

    working homepage

---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,9 @@
+@extends('site.layouts.basic')
+
+@section('titulo', 'Home')
+
+@section('conteudo')
+
+<div>
+    <h1>Biblioteca</h1>
+</div>
---
+++ resources/views/site/layouts/_partials/top.blade.php
@@ -0,0 +1,14 @@
+<div class="top">
+    <div class="logo">
+{{--        colocar uma imagem--}}
+        <p>Logo</p>
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a href="{{ route('site.index') }}">Home</a></li>
+            <li><a href="{{ route('site.index') }}">Sobre</a></li>
+            <li><a href="{{ route('site.index') }}">Contato</a></li>
+        </ul>
+    </div>
+</div>
